6f8a21fc3e8e39ddaa6709e4f134858cc908559e
atharvsalunke2005-cell <atharvsalunke2005@gmail.com>
2026-06-13 12:11:34 +0530

    modified app.js again

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hii from jenkins, added webhook, we are from 18 May Devops batch');
+  res.send('Welcome to FCT, Hello from jenkins, added webhook, we are from 18 May Devops batch');
 });
 
 app.listen(port, () => {
